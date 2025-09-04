147619b7bf338add7ec33d4955d481810c8cd3c7
Bruno Ferreira <139245933+bruno-ferreira-97@users.noreply.github.com>
2025-09-04 20:59:07 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+
+function abrirWhatsApp() {
+  const numeroTelefone = '5515997507811';
+  const mensagem = 'Olá, gostaria de mais informações sobre agendamento!';
+  const mensagemCodificada = encodeURIComponent(mensagem);
+  const urlWhatsApp = `https://wa.me/${numeroTelefone}?text=${mensagemCodificada}`;
+  window.open(urlWhatsApp, '_blank');
+}
