f72e9a06de41b5c4f9328a23f76f0c247a2a4bd4
Bruno Ferreira <139245933+bruno-ferreira-97@users.noreply.github.com>
2025-11-18 15:20:06 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 
 function abrirWhatsApp(){
-  const numeroTelefone = '5515997507811';
+  const numeroTelefone = '5515997937575';
   const mensagem = 'Olá, gostaria de mais informações sobre agendamento!';
   const mensagemCodificada = encodeURIComponent(mensagem);
   const urlWhatsApp = `https://wa.me/${numeroTelefone}?text=${mensagemCodificada}`;
